86716f1eec8adea634fd507e533a26d724966589
Anatoly Ryumkin <aryumkin@deloitte.com>
2020-10-21 23:49:23 +0900

    Update Past games page, add teacher and players info

--- main/pages/PPastGames/index.js
+++ main/pages/PPastGames/index.js
@@ -9,9 +9,63 @@ import './index.styl'
 
 const PPastGames = () => {
   const [user] = useSession('user')
-  const query = user.isTeacher ? { teacherId: { $in: [user.id] } } : { playersIds: { $in: [user.id] } }
-  const [games = []] = useQueryTable('games', {
-    query: { ...query, isFinished: true }
+  const search = user.isTeacher ? { teacherId: { $in: [user.id] } } : { playersIds: { $in: [user.id] } }
+  const [games = {}] = useQueryTable('games', {
+    query: [
+      {
+        $match: { isFinished: true, ...search }
+      },
+      {
+        $lookup: {
+          from: 'rounds',
+          let: { lastIndex: '$lastFinishedRoundIndex', gameId: '$_id' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [{ $eq: ['$$lastIndex', '$gameIndex'] }, { $eq: ['$$gameId', '$gameId'] }]
+                }
+              }
+            }
+          ],
+          as: 'round'
+        }
+      },
+      { $unwind: { path: '$round', preserveNullAndEmptyArrays: true } },
+      {
+        $lookup: {
+          from: 'users',
+          let: { playersIds: '$playersIds' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $in: ['$_id', '$$playersIds']
+                }
+              }
+            }
+          ],
+          as: 'players'
+        }
+      },
+      {
+        $lookup: {
+          from: 'users',
+          let: { teacherId: '$teacherId' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: ['$_id', '$$teacherId']
+                }
+              }
+            }
+          ],
+          as: 'teacher'
+        }
+      },
+      { $unwind: { path: '$teacher', preserveNullAndEmptyArrays: true } }
+    ]
   })
   const [expandedGameId, $expandedGameId] = useValue()
 
@@ -27,6 +81,16 @@ const PPastGames = () => {
       `
     },
     {
+      title: 'Teacher',
+      key: 'teacher',
+
+      ellipsis: true,
+      align: 'center',
+      render: (data) => pug`
+        Span.line.text #{data.teacher.name}
+      `
+    },
+    {
       title: 'Created At',
       key: 'age',
 
@@ -36,13 +100,28 @@ const PPastGames = () => {
       `
     },
     {
-      title: 'Players count',
-      key: 'playersCount',
-
-      align: 'center',
-      render: (data) => pug`
-        Span.line.text #{data.playersIds.length}
-      `
+      title: 'Player1',
+      key: 'player1',
+      render: (data) => {
+        const info = data.round.players[data.playersIds[0]]
+        const player = data.players.find((item) => item._id === data.playersIds[0]) || {}
+        return pug`
+          Span.line.text #{player.name}
+          Span.line.text #{info.points}
+        `
+      }
+    },
+    {
+      title: 'Player2',
+      key: 'player2',
+      render: (data) => {
+        const info = data.round.players[data.playersIds[1]]
+        const player = data.players.find((item) => item._id === data.playersIds[1]) || {}
+        return pug`
+          Span.line.text #{player.name}
+          Span.line.text #{info.points}
+        `
+      }
     }
   ]
 
@@ -52,7 +131,7 @@ const PPastGames = () => {
   `
 
   const handleExpand = (expanded, record) => {
-    $expandedGameId.set(expanded ? record.id : null)
+    $expandedGameId.set(expanded ? record._id : null)
   }
 
   return pug`
@@ -64,7 +143,7 @@ const PPastGames = () => {
             dataSource=games.items
             pagination=games.pagination
             columns=columns
-            rowKey=item => item.id
+            rowKey=item => item._id
             expandedRowKeys=expandedGameId?[expandedGameId]:[]
             onExpand=handleExpand
             expandedRowRender=rowExpandRender
